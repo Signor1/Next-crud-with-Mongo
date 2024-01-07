70f5cc4a40e25f6adf68e5b0a5074ee15e73cf91
signor1 <emmanuelomemgboji@gmail.com>
2024-01-07 09:57:34 +0100

    feat: added navbar

---
+++ components/NavBar.jsx
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+import React from 'react'
+
+const NavBar = () => {
+    return (
+        <nav className='w-full flex bg-purple-600 justify-between items-center h-16 px-8'>
+            <Link className='text-white font-bold' href={`/`}>CRUD</Link>
+            <Link className='bg-gray-200 text-purple-600 px-6 py-2 border-none outline-none text-sm rounded-sm hover:bg-gray-800 hover:text-gray-200' href={`/addTopic`}>Add Topic</Link>
+        </nav>
+    )
+}
+
+export default NavBar
